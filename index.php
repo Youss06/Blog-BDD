76418bfed9244f8cdd11c35539ed1e9da916e63c
Youss06 <oumar.youssouf0605@gmail.com>
2017-09-14 15:08:42 +0200

    Add h1 and p

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Mon Blog</title>
+  </head>
+  <body>
+<h1>Mon super Blog !</h1>
+<p>Derniers billets du blog :</p>
+
+
+
+
+  </body>
+</html>
